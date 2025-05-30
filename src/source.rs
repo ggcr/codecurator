ba882b610c252f5d60c46cc696bbb855f4b20908
Cristian Gutiérrez <ggcristian@icloud.com>
2025-05-30 15:32:57 +0200

    Simplified `parse_source` method

--- src/source.rs
+++ src/source.rs
@@ -1,22 +1,23 @@
 use std::fs;
 use std::path::PathBuf;
 
+use itertools::Itertools;
+use serde_json::Error;
+
+fn parse_line(line: Result<&str, Error>) -> Option<(&str, &str)> {
+    match line {
+        Ok(line) => line.splitn(2, "/").collect_tuple(),
+        _ => None,
+    }
+}
+
 pub fn parse_source(source: &PathBuf) -> Vec<(String, String)> {
     let input = fs::read_to_string(source).expect("Cannot read source");
     let mut valid_repos: Vec<(String, String)> = Vec::new();
-
     for line in serde_json::Deserializer::from_str(&input).into_iter::<&str>() {
-        let repo = match line {
-            Ok(line) => line,
-            _ => continue,
-        };
-        let parts: Vec<&str> = repo.split("/").collect();
-        if parts.len() == 2 {
-            valid_repos.push((parts[0].to_owned(), parts[1].to_owned()));
+        if let Some((user, repo)) = parse_line(line) {
+            valid_repos.push((user.to_owned(), repo.to_owned()));
         }
     }
-    // for repo in &valid_repos {
-    //     println!("{}/{}", repo.0, repo.1);
-    // }
     valid_repos
 }
