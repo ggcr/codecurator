fcf61769bb2f8e0576d5af0af34ce40184715aa2
Cristian Gutiérrez <ggcristian@icloud.com>
2025-06-11 09:34:30 +0200

    Add script to count chars estimating the length of the k-shingles (n-grams)

---
+++ stats/count_chars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+if [ -z "$1" ]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+DIR="$1"
+total=0
+for file in "$DIR"/*.jsonl; do
+    char_count=$(jq -s '[.[] | .text] | length' "$file")
+    total=$((total + char_count))
+done
+
+echo "Total chars: $total"
+
